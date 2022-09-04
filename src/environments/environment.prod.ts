e9c3769c0abf0672e9af73f6dc2e3696a0631395
Jd4rc3 <jdarce91@misena.edu.co>
2022-09-04 07:45:19 -0500

    Firebase dependencies added
    Firebase dependencies added with "ng add command @angular/fire"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'appcards-17a89',
+    appId: '1:815472592240:web:8af161f3a903453872bbb5',
+    storageBucket: 'appcards-17a89.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAzG3eZTrQqymnMZ1pydpTufzh5nZGg6X4',
+    authDomain: 'appcards-17a89.firebaseapp.com',
+    messagingSenderId: '815472592240',
+    measurementId: 'G-X42EQ5R73M',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'appcards-17a89',
+    appId: '1:815472592240:web:8af161f3a903453872bbb5',
+    storageBucket: 'appcards-17a89.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAzG3eZTrQqymnMZ1pydpTufzh5nZGg6X4',
+    authDomain: 'appcards-17a89.firebaseapp.com',
+    messagingSenderId: '815472592240',
+    measurementId: 'G-X42EQ5R73M',
+  },
   production: false
 };
 
