ea04ee1918a9be9eff9e2e39a0282f87e809f1a2
Jd4rc3 <jdarce91@misena.edu.co>
2022-09-12 23:53:22 -0500

    Navbar dynamic hidden

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Player } from '../../player.model';
 import { AuthService } from '../../../login/services/auth.service';
 import { MenuItem } from 'primeng/api';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -11,6 +12,7 @@ import { MenuItem } from 'primeng/api';
 export class NavbarComponent implements OnInit {
   items: MenuItem[] = [];
   visible: boolean = true;
+  inBoard: boolean = false;
 
   constructor(private authService: AuthService) {
     this.authService.onAuthStateChanged((user) =>
@@ -22,6 +24,10 @@ export class NavbarComponent implements OnInit {
     this.items = [
       { label: 'Home', icon: 'pi pi-fw pi-home', routerLink: '/game' },
     ];
+
+    window.location.pathname.includes('board')
+      ? (this.inBoard = true)
+      : (this.inBoard = false);
   }
 
   logout() {
